9a5ec6d00549cdc70a5fa6cf2a9758c0fdfaedd2
Julien Verlaguet <julien.verlaguet@gmail.com>
2014-02-03 20:11:03 -0800

    Update index.php
    Feel free to ignore these. Let me know if you want access to the type-checker, it should be released soon anyway.

--- www/types/index.php
+++ www/types/index.php
@@ -16,13 +16,13 @@ echo_upper("lower"); // LOWER
 echo_upper(5);  // ERROR(calling "echo_upper()" on l.16) : Argument 1 passed to echo_upper() must be an instance of string, int given
 echo_upper(null); // ERROR(calling "echo_upper()" on l.17) : Argument 1 passed to echo_upper() must be an instance of string, null given
 
-function sum(array<int> $a) {
+function sum(array<int> $a): int {
     return array_reduce($a, ($sum, $i) ==> $sum + $i);
 }
 
 echo <p>sum([1, 2, 3]) = {sum([1, 2, 3])}</p>; // sum([1, 2, 3]) = 6
 
-class Generics<T as countable> {
+class Generics<T as num> {
     private ?T $t;
 
     public function get() : T {
@@ -33,16 +33,16 @@ class Generics<T as countable> {
         $this->t = $t;
     }
 
-    public function __toString() {
+    public function __toString(): string {
         return var_export($this->t, true);
     }
 
-    public function count() {
+    public function count(): int {
         return $this->t->count();
     }
 }
 
-$type = new Generics<Vector<int>>();
+$type = new Generics();
 $type->set(Vector {1, 2, 3});
 
 echo <div>$type
@@ -51,7 +51,11 @@ echo <div>$type
         <li>size = {$type->count()}</li> <!-- 3 -->
     </ul></div>;
 
-// To keep performance at a top level, there is no runtime type check
+// To keep performance at a top level, the runtime check is partial.
+// The runtime check doesn't iterate on objects. In the case of array<int>
+// passing an int would be caught both at runtime and by the type-checker,
+// however passing an array of strings (like in this case) would only be
+// caught by the type-checker.
 // Those programming errors would be caught by the (yet to be release)
 // static analyzer
 sum(["a", "b"]);
